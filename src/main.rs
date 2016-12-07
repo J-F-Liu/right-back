a5507c8da33c5a0e0d4d4c735612861321a4b1af
Junfeng Liu <china.liujunfeng@gmail.com>
2016-12-07 14:29:57 +0800

    use clap to get port number from cli args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate hyper;
-
-use std::time::{SystemTime, UNIX_EPOCH};
 use hyper::server::{Server, Request, Response};
 
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg};
+
+use std::time::{SystemTime, UNIX_EPOCH};
 static mut startup_time: SystemTime = UNIX_EPOCH;
 
 fn message(req: Request, res: Response) {
@@ -24,7 +27,24 @@ fn message(req: Request, res: Response) {
 }
 
 fn main() {
-    println!("Start right-back server...");
+    let matches = App::new("right-back web server")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .arg(Arg::with_name("port")
+            .short("p")
+            .long("port")
+            .value_name("PORT")
+            .help("Sets http port number")
+            .takes_value(true)
+            .use_delimiter(false)
+            .default_value("80"))
+        .get_matches();
+
     unsafe { startup_time = SystemTime::now(); }
-    Server::http("0.0.0.0:3000").unwrap().handle(message).unwrap();
+
+    if let Some(port) = matches.value_of("port") {
+        let address = format!("0.0.0.0:{}", port);
+        println!("right-back web server is running at http://{}/", address);
+        Server::http(address.as_str()).unwrap().handle(message).unwrap();
+    }
 }
