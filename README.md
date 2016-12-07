7ab34cc3f1ba32d721fe0781c018e48d90ce16cd
Junfeng Liu <china.liujunfeng@gmail.com>
2016-12-07 15:39:41 +0800

    use maud to generate html code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # right-back
 A simple standalone web server to be used when your website is during maintenance.
+
+## Run code
+```
+pacman -S rustup
+rustup install nightly
+rustup default nightly
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![feature(plugin)]
+#![plugin(maud_macros)]
+extern crate maud;
+
 extern crate hyper;
 use hyper::server::{Server, Request, Response};
 
@@ -6,28 +10,44 @@ extern crate clap;
 use clap::{App, Arg};
 
 use std::time::{SystemTime, UNIX_EPOCH};
-static mut startup_time: SystemTime = UNIX_EPOCH;
+static mut STARTUP_TIME: SystemTime = UNIX_EPOCH;
 
-fn message(req: Request, res: Response) {
-   unsafe { match startup_time.elapsed() {
-       Ok(elapsed) => {
-           let minutes = elapsed.as_secs() / 60;
-           if minutes <= 5 {
-               res.send(format!("网站维护中，请稍等{}分钟后回来！", 5 - minutes).as_bytes());
-           } else if minutes > 15 {
-               res.send("网站维护中，请等30分钟后回来！".as_bytes());
-           } else {
-               res.send("网站维护中，请稍等片刻！".as_bytes());
-           }
-       }
-       Err(e) => {
-           res.send(format!("Error: {:?}", e).as_bytes());
-       }
-   }}
+fn message(_: Request, res: Response) {
+    unsafe {
+        let message = match STARTUP_TIME.elapsed() {
+            Ok(elapsed) => {
+                let minutes = elapsed.as_secs() / 60;
+                if minutes < 5 {
+                    format!("网站维护中，请稍等{}分钟后回来！", 5 - minutes)
+                } else if minutes > 15 {
+                    "网站维护中，请等30分钟后回来！".to_string()
+                } else {
+                    "网站维护中，请稍等片刻！".to_string()
+                }
+            }
+            Err(e) => {
+                format!("Error: {:?}", e)
+            }
+        };
+
+        let markup = html! {
+          html {
+            head {
+              meta charset="utf-8" /
+              title "云路网"
+            }
+            body {
+              p (message)
+            }
+          }
+        };
+
+        res.send(markup.into_string().as_bytes()).unwrap();
+    }
 }
 
 fn main() {
-    let matches = App::new("right-back web server")
+    let arguments = App::new("right-back web server")
         .version(crate_version!())
         .author(crate_authors!())
         .arg(Arg::with_name("port")
@@ -40,9 +60,9 @@ fn main() {
             .default_value("80"))
         .get_matches();
 
-    unsafe { startup_time = SystemTime::now(); }
+    unsafe { STARTUP_TIME = SystemTime::now(); }
 
-    if let Some(port) = matches.value_of("port") {
+    if let Some(port) = arguments.value_of("port") {
         let address = format!("0.0.0.0:{}", port);
         println!("right-back web server is running at http://{}/", address);
         Server::http(address.as_str()).unwrap().handle(message).unwrap();
